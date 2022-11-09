8d24a75e48295f5d945ec79cc22006348bed48bc
Tomas Oliveira <tomasoliveira31@gmail.com>
2022-11-09 21:42:26 +0000

    ADD: App frontend

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const useStyles = makeStyles(() => ({
 
   title: {
     flex: 1,
-    color: "black",
+    color: "white",
     fontfamily: 'Island Moments',
     fontWeight: "bold",
     cursor: "pointer",
