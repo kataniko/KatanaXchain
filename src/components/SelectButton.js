583ffedb55ce98131f6cb16882dbd2684d05f8ce
kataniko <tomasoliveira31@gmail.com>
2023-01-02 09:27:29 +0000

    ADD: main frontend

---
+++ src/components/SelectButton.js
@@ -0,0 +1,34 @@
+import { makeStyles } from "@material-ui/core";
+
+const SelectButton = ({ children, selected, onClick }) => {
+  const useStyles = makeStyles({
+    selectbutton: {
+      border: "1px solid green",
+      borderRadius: 5,
+      padding: 10,
+      paddingLeft: 20,
+      paddingRight: 20,
+      fontFamily: "Montserrat",
+      cursor: "pointer",
+      backgroundColor: selected ? "green" : "",
+      color: selected ? "black" : "",
+      fontWeight: selected ? 700 : 500,
+      "&:hover": {
+        backgroundColor: "green",
+        color: "black",
+      },
+      width: "22%",
+      //   margin: 5,
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <span onClick={onClick} className={classes.selectbutton}>
+      {children}
+    </span>
+  );
+};
+
+export default SelectButton;
